167884ba3956793504e67b76c6775e36e241423c
Aria <108920295+aria973@users.noreply.github.com>
2026-03-08 18:14:26 +0330

    Update app.js

--- app.js
+++ app.js
@@ -172,7 +172,7 @@ renderHistory();
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js").catch(() => {});
+    navigator.serviceWorker.register('/Aralaps/sw.js').catch(() => {});
   });
 }
 
